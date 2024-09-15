125e9fbba332e45773888c06fc385a0ac217da4d
liran friedman <friedmanliran@gmail.com>
2024-09-15 23:29:07 +0300

    remove shit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuTrigger,
-  NavigationMenuContent,
-  NavigationMenuLink,
-} from "./components/ui/navigation-menu.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
