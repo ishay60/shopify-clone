8fc8c82fcc0c1a7ec9c5d8499abdd08b965bf8c2
Ishay Rosengarten <ishay60@gmail.com>
2024-09-16 11:43:34 +0300

    first commit

---
+++ src/ProductCarousel.jsx
@@ -0,0 +1,22 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+function ProductCarousel() {
+  return (
+    <Carousel>
+      <CarouselContent>
+        <CarouselItem>...</CarouselItem>
+        <CarouselItem>...</CarouselItem>
+        <CarouselItem>...</CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
+export default ProductCarousel;
